f620086ad0a8268f2fbdb4da5c539318759c1aa6
Mo Hydar <mohydar@protonmail.com>
2018-08-07 00:00:30 +0100

    Created card variables

---
+++ scripts/main.js
@@ -0,0 +1,62 @@
+// Card Data 
+const cardArray[{
+        'name': 'shell',
+        'img': 'media/images/blueshell.png',
+    },
+
+    {
+        'name': 'star',
+        'img': 'media/images/star.png',
+    },
+
+    {
+        'name': 'bobomb',
+        'img': 'media/images/bobomb.png',
+    },
+
+    {
+        'name': 'mario',
+        'img': 'media/images/mario.png',
+    },
+
+    {
+        'name': 'luigi',
+        'img': 'media/images/luigi.png',
+    },
+
+    {
+        'name': 'peach',
+        'img': 'media/images/peach.png',
+    },
+
+    {
+        'name': '1up',
+        'img': 'media/images/1up.png',
+    },
+
+    {
+        'name': 'mushroom',
+        'img': 'media/images/mushroom.png',
+    },
+
+    {
+        'name': 'thwomp',
+        'img': 'media/images/thwomp.png',
+    },
+
+    {
+        'name': 'bulletbill',
+        'img': 'media/images/bulletbill.png',
+    },
+
+    {
+        'name': 'coin',
+        'img': 'media/images/coin.png',
+    },
+
+    {
+        'name': 'goomba',
+        'img': 'media/images/goomba.png',
+    },
+]
+
